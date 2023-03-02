05f8cc52e0795b2b2313e31a333d7a08fc4665d7
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-02 15:59:12 +0100

    title white

--- resources/views/admin/movies/create.blade.php
+++ resources/views/admin/movies/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container">
+<div class="container text-white">
     <div class="row">
         <div class="col-12 text-center m-4">
             <h2 class="text-white">Aggiungi nuovo movies</h2>
@@ -19,10 +19,37 @@
             </div>
             <div class="form-group mb-3">
                 <label class="control-label mb-2">
+                    Original Titolo
+                </label>
+                <input type="text" class="form-control" placeholder="Titolo" id="original_title" name ="original_title">
+                    @error('original_title')
+                    <div class="alert alert-danger mt-2">{{$message}}</div>
+                    @enderror
+            </div>
+            <div class="form-group mb-3">
+                <label class="control-label mb-2">
+                    Vote
+                </label>
+                <input type="number" class="form-control" placeholder="Titolo" id="vote" name ="vote">
+                    @error('vote')
+                    <div class="alert alert-danger mt-2">{{$message}}</div>
+                    @enderror
+            </div>
+            <div class="form-group mb-3">
+                <label class="control-label mb-2">
+                    Cover
+                </label>
+                <input type="text" class="form-control" placeholder="Titolo" id="cover_path" name ="cover_path">
+                    @error('cover_path')
+                    <div class="alert alert-danger mt-2">{{$message}}</div>
+                    @enderror
+            </div>
+            <div class="form-group mb-3">
+                <label class="control-label mb-2">
                     Data
                 </label>
-                <input type="date" class="form-control" placeholder="Data" name="date_project">
-                    @error('date_project')
+                <input type="date" class="form-control" placeholder="Data" name="release_data">
+                    @error('release_data')
                         <div class="alert alert-danger mt-2">{{$message}}</div>
                     @enderror
             </div>
@@ -30,7 +57,7 @@
                 <label class="control-label mb-2">
                     Contenuto
                 </label>
-                <textarea type="text-area" class="form-control" placeholder="Contenuto" id="content" name ="content"></textarea>
+                <textarea type="text-area" class="form-control" placeholder="Contenuto" id="cast" name ="cast"></textarea>
             </div>
             <div class="form-group mb-3">
                 <button type="submit" class="btnblue">Salva</button>
