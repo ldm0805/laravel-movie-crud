9d45131fca63d043370081ba857f39610306708a
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-02 13:33:21 +0100

    Modifica card completa: -aggiunto flex column;

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -6,14 +6,14 @@
 <div id="card-template" class="cellphone-container d-flex flex-wrap">
       
     @foreach ($movies as $item)
-                <div class="movie">       
-                  <div class="movie-img">
+                <div class="movie d-flex flex-column justify-content-between ">       
+                  <div class="movie-img ">
                     <img src="{{ $item['cover_path'] }}" alt="Card image cap">
                   </div>
-                  <div class="text-movie-cont">
+                  <div class="text-movie-cont mb-auto p-2">
                     <div class="mr-grid">
-                      <div class="">
-                        <h3 class="mt-6">{{ $item['title'] }}</h3>
+                      <div class="align-top">
+                        <h3>{{ $item['title'] }}</h3>
                         <ul class="movie-gen">
                             <li>{{ $item['release_date'] }}</li>
                           <li>{{ $item['nationality'] }}</li>
@@ -33,16 +33,16 @@
                           {{ $item['cast'] }}
                         </div>
                     </div>
-                    <div class="d-flex">
-                      <a class="btn btn-outline-light m-2" href="{{ route('admin.movies.show', ['movie' => $item['id']]) }}"><i class="fa-solid fa-circle-info"></i> info</a>
-                        <form action="{{route('admin.movies.destroy', ['movie' => $item['id']] )}}" method="POST">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit" class="btn btn-outline-danger m-2">
-                            <i class="fa fa-trash"></i> Elimina
-                        </button>
-                        </form>
-                    </div>
+                  </div>
+                  <div class="d-flex">
+                    <a class="btn btn-outline-light m-2" href="{{ route('admin.movies.show', ['movie' => $item['id']]) }}"><i class="fa-solid fa-circle-info"></i> info</a>
+                      <form action="{{route('admin.movies.destroy', ['movie' => $item['id']] )}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-outline-danger m-2">
+                          <i class="fa fa-trash"></i> Elimina
+                      </button>
+                      </form>
                   </div>
                 </div>
                 @endforeach
