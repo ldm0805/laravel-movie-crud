e88e777caa29aa6aadff41772cd3d951343d0d27
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-07 15:14:34 +0100

    ultimo branch

--- database/seeders/CastSeeder.php
+++ database/seeders/CastSeeder.php
@@ -13,17 +13,15 @@ class CastSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        $casts = ['Tom Hanks', 'Meryl Streep', 'Denzel Washington', 'Viola Davis', 'Anthony Hopkins', 'Emma Stone', 'Leonardo DiCaprio', 'Jennifer Lawrence', 'Brad Pitt', 'Angelina Jolie', 'Charlize Theron', 'Matt Damon', 'Julia Roberts', 'George Clooney', 'Cate Blanchett', 'Robert De Niro', 'Al Pacino', 'Jack Nicholson', 'Dustin Hoffman', 'Tom Cruise', 'Nicole Kidman', 'Kate Winslet', 'Johnny Depp', 'Halle Berry', 'Samuel L. Jackson', 'Will Smith', 'Jodie Foster', 'Kevin Spacey', 'Robin Williams', 'Sean Connery', 'Morgan Freeman', 'Mel Gibson', 'Sandra Bullock', 'Jim Carrey', 'Keanu Reeves', 'Mark Wahlberg', 'Scarlett Johansson', 'Chris Evans', 'Ryan Reynolds', 'Gal Gadot', 'Robert Downey Jr.'];
-
-        foreach($casts as $cast){
+        $casts = config('actorArr.actors');
+        foreach ($casts as $cast) {
             $newCast = new Cast();
             $newCast->nome_cognome = $cast;
             $newCast->slug = Cast::generateSlug($cast);
             $newCast->save();
-
         }
-        
     }
 }
